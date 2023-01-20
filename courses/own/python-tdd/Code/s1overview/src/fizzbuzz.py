b919f78b4029d38883c1ff6c987dcceca87fb6ef
Tom Ordonez <tomordonezworks@gmail.com>
2023-01-20 14:42:26 -0600

    python-tdd/1: Refactor if statements

--- courses/own/python-tdd/Code/s1overview/src/fizzbuzz.py
+++ courses/own/python-tdd/Code/s1overview/src/fizzbuzz.py
@@ -13,11 +13,11 @@ class FizzBuzz:
         self._number = number
 
     def calculate_fizzbuzz(self):
-        if (self._number % 15 == 0):
+        if (self._number % 15) == 0:
             return "FizzBuzz"
-        elif (self._number % 3 == 0):
+        elif (self._number % 3) == 0:
             return "Fizz"
-        elif (self._number % 5 == 0):
+        elif (self._number % 5) == 0:
             return "Buzz"
         else:
             return str(self._number)
