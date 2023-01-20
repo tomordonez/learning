76f1864e0f8ec17f2b15892257812ea71d180ca4
Tom Ordonez <tomordonezworks@gmail.com>
2023-01-20 11:31:33 -0600

    python-tdd/1: Pass test to get "1" when passing 1

--- courses/own/python-tdd/Code/s1overview/tests/test_fizzbuzz.py
+++ courses/own/python-tdd/Code/s1overview/tests/test_fizzbuzz.py
@@ -3,9 +3,17 @@ import unittest
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_FizzBuzz_class_exists(self):
-        fizzbuzz = FizzBuzz()
-        self.assertTrue(isinstance(fizzbuzz, FizzBuzz))
+
+    def setUp(self) -> None:
+        self.fizzbuzz = FizzBuzz()
+
+    def test_can_create_fizzbuzz_instance(self):
+        self.assertTrue(isinstance(self.fizzbuzz, FizzBuzz))
+
+    def test_return_string_one_when_passing_int_one(self):
+        self.fizzbuzz.number = 1
+        self.assertEqual("1", self.fizzbuzz.number)
+
 
 if __name__ == "__main__":
     unittest.main()
